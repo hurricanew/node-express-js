9ddbf994e1a084760eb602ad34ce73c7a8387af5
hurricanew <hurricanew@hotmail.com>
2019-05-14 06:37:42 +1000

    hello-world-1
    https://codesandbox.io/s/m5v2k7v0jp

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var http = require("http");
 var app = new express();
 //create a server object:
 app.use(function(req, res) {
-  res.write("Hello World!"); //write a response to the client
+  res.write("Hello World !"); //write a response to the client
   res.end(); //end the response
 });
 http.createServer(app).listen(8080); //the server object listens on port 8080
